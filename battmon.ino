6acf8c6777a510de2d0e76f5ec2e1a86c7c2550a
Loic POLLIER <loic.pollier@gmail.com>
2020-10-18 17:24:44 +0200

    Update the release-2.0 branch for battmon 2.0.2 integration

--- battmon.ino
+++ battmon.ino
@@ -4,7 +4,7 @@
   
   I2C/SMBus interface to read Smart Battery data with Arduino Uno and LCD keypad shield
   Monitors data from bq2040 gas gauge equipped in Lithium Ion Battery Pack such as for Aibo ERS2xx (ERA-201B1), ERS3xx (ERA-301B1) and ERS7 (ERA-7B1/ERA-7B2) series
-  Last update: 2020-10-08 released version 2.0.1
+  Last update: 2020-10-18 released version 2.0.2
   
   Made with Arduino Uno R3
   
